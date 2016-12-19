ec65cf2637d702f0fa07c45b27883639ab11a827
Neo <wenchen.li@jam3.com>
2016-12-19 14:55:47 -0500

    Add function parse

--- src/parse.js
+++ src/parse.js
@@ -1,4 +1,6 @@
-function parse(text) {
+function parse(text, pattern = '\\*\\*', tag = 'strong') {
+  const regexp = new RegExp(`(${pattern})(.*?)\\1`, 'g');
+  return text.replace(regexp, `<${tag}>$2</${tag}>`);
 }
 
 module.exports = parse;
